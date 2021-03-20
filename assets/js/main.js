feccdeff73710ccaf4251fd16d61af9d44e229b8
suchiz <kyuuzuka@hotmail.fr>
2021-03-20 18:56:12 +0100

    sd

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,49 +26,91 @@
 			}, 100);
 		});
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			speed: 300,
-			alignment: 'center'
-		});
+		var $mainButton = $('#mainButton');
+				
 
-	// Scrolly
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height() - 5; }
-		});
+		if ($mainButton.length > 0 || $nav.length > 0) {
 
-	// Nav.
+			// Shrink effect.
+				$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$mainButton.addClass('alt');
+						},
+						leave: function() {
+							$mainButton.addClass('alt');
+						},
+					});
 
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
+				
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
+			// Links.
+				var $mainButton_a = $mainButton.find('a');
+
+				$mainButton_a 
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $mainButton.height() - 150 ; }
+					})
+					.on('click', function() {
+
+						var $this = $(this);
+
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
+
+						// Deactivate all links.
+							$mainButton_a
+								.removeClass('active')
+								.removeClass('active-locked');
+
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
+
+					})
+					.each(function() {
+
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
+
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
+
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
+
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
+								},
+								enter: function() {
+									// Activate section.
+										$section.removeClass('inactive');
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($mainButton_a.filter('.active-locked').length == 0) {
+
+											$mainButton_a.removeClass('active');
+											$this.addClass('active');
+
+										} 
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
+								}
+							});
+					});
+				}
+
+				$('.scrolly').scrolly({
+					speed: 1000
 				});
 
 })(jQuery);
