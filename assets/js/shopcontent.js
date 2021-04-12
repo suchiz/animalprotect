de883470b429a96b2aa9191b3458873f5a6307e8
suchiz <kyuuzuka@hotmail.fr>
2021-04-12 17:36:34 +0200

    push

--- assets/js/shopcontent.js
+++ assets/js/shopcontent.js
@@ -8,14 +8,30 @@ var prevButton = document.getElementById("prevButton");
 var nextButton = document.getElementById("nextButton");
 var productTitle = document.getElementById("productTitle");
 
+
+
 fetch('https://spreadsheets.google.com/feeds/cells/1yKszxtG9r5PM_YPjRIHP4WpwBaEr6KYo24-LdIs7T3Y/1/public/full?alt=json').then(
     function (response){ return response.json();}
 ).then(function(obj){
     // console.log(obj.feed.entry[0]);
     // document.getElementById("shopContent").innerHTML = `
-  inflate(obj.feed.entry, 1);
-  alldata = obj.feed.entry;
-  productTitle.innerHTML = `TOUS NOS PRODUITS <span style="font-size: 0.8em">(${alldata.length/6}  produits)</span> `;
+    alldata = obj.feed.entry;
+    if (window.location.hash === '#CHIEN') {displayCategory('CHIEN')}
+    else if (window.location.hash === '#CHAT') {displayCategory('CHAT')}
+    else if (window.location.hash === '#RONGEUR') {displayCategory('RONGEUR')}
+    else if (window.location.hash === '#RAFRAICHISSANT') {displayCategory('RAFRAICHISSANT')}
+    else if (window.location.hash === '#HYGIENE') {displayCategory('HYGIENE')}
+    else if (window.location.hash === '#VOITURE') {displayCategory('VOITURE')}
+    else if (window.location.hash === '#EDUCATION') {displayCategory('EDUCATION')}
+    else if (window.location.hash === '#MANTEAU') {displayCategory('MANTEAU')}
+    else if (window.location.hash === '#PULL') {displayCategory('PULL')}
+    else if (window.location.hash === '#CANICROSS') {displayCategory('CANICROSS')}
+    else if (window.location.hash === '#AQUATIQUE') {displayCategory('AQUATIQUE')}
+    else{
+        inflate(alldata, 1);
+        productTitle.innerHTML = `TOUS NOS PRODUITS <span style="font-size: 0.8em">(${alldata.length/6}  produits)</span> `;
+    }
+
 
 }).catch(function(error){
     console.log(error);
@@ -26,7 +42,7 @@ function displayCategory(category){
     
     alldata.forEach((item, index) => {
         if(index%6 == 0){
-            console.log(alldata[index+3].content.$t);
+           
             if(alldata[index+3].content.$t == category){
                 for(var i = 0; i < 6; i++){
                     filteredArray.push(alldata[index+i]);
